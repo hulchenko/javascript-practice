fa252f354efd708f6e54264fcd84e4da3770bb4c
Vadym <hulchenko@shaw.ca>
2021-01-13 19:47:49 -0700

    create dir for simple calendar project, rename theme file to background-theme, minor fixes

--- practice.js
+++ practice.js
@@ -1,8 +1,29 @@
 <script>
-    let myInt = 5;
-    let myFloat = 6.667;
-    myInt;
-    myFloat;
-    typeof myInt;
-    typeof myFloat;
-    </script>
+      const select = document.querySelector('select');
+      const list = document.querySelector('ul');
+      const h1 = document.querySelector('h1');
+      
+      select.onchange = function() {
+        const choice = select.value;
+        let days = 31;
+        if (choice === "February") {
+          days = 28;
+        }
+        else if (choice === "September" || choice === "April" || choice === "June" || choice === "November") {
+          days = 30;
+        }
+        createCalendar(days, choice);
+      }
+      
+      function createCalendar(days, choice) {
+        list.innerHTML = '';
+        h1.textContent = choice;
+        for (let i = 1; i <= days; i++) {
+          const listItem = document.createElement('li');
+          listItem.textContent = i;
+          list.appendChild(listItem);
+        }
+      }
+      
+      createCalendar(31,'January');
+</script>
