ba4a4f4ac4d7adb4010f832d245697d5e32402d7
Vadym <hulchenko@shaw.ca>
2021-01-20 21:18:53 -0700

    add new directories, cleaner code, new files

---
+++ guest-list/guest-list.js
@@ -0,0 +1,11 @@
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+const admitted = document.querySelector('.admitted');
+const refused = document.querySelector('.refused');
+admitted.textContent = 'Admit: ';
+refused.textContent = 'Refuse: '
+
+let i = 0;
+
+// refused.textContent += ;
+// admitted.textContent += ;
