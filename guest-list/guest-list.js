44d5e64942719a2aad1f56351716bba6807984db
Vadym <hulchenko@shaw.ca>
2021-01-20 23:38:57 -0700

    add for loop

--- guest-list/guest-list.js
+++ guest-list/guest-list.js
@@ -21,3 +21,13 @@ while (i<people.length) {
     refused.textContent = refused.textContent.slice(0, refused.textContent.length-2) + ".";
     admitted.textContent = admitted.textContent.slice(0, admitted.textContent.length-2) + ".";
 }
+
+//added "for" loop, as an alternative:
+//for (let i = 0; i < people.length; i++) {
+//    if (people[i] === "Lola" || people[i] === "Phil") {
+//        refused.textContent += people[i] + ", ";
+//    }else{
+//        admitted.textContent += people[i] + ", ";
+//    }
+//}
+
